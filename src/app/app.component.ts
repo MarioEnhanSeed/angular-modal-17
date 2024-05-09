2160892551c3b91c3cec0784eca56b2be2a7356d
MarioEnhanSeed <mario_enhanseed@outlook.com>
2024-05-08 23:00:23 -0300

    correção de código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,10 +17,8 @@ import {
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('modal') public modalTemplateRef!: TemplateRef<any>;
-  @ViewChild('template1') public template1?: TemplateRef<any>;
-  @ViewChild('template2') public template2?: TemplateRef<any>;
 
-  public selectedTemplate?: TemplateRef<any>;
+  public selectedTemplate!: TemplateRef<any>;
 
   title = 'angular-modal-17';
   firstName = 'Mario Henrique';
@@ -34,12 +32,10 @@ export class AppComponent implements AfterViewInit {
     private cd: ChangeDetectorRef
   ) {}
   ngAfterViewInit(): void {
-    this.selectedTemplate = this.template1;
     this.cd.detectChanges();
   }
 
   public show() {
-    this.selectedTemplate = this.template2;
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details',
--- src/app/shared/components/modal/modal.service.ts
+++ src/app/shared/components/modal/modal.service.ts
@@ -1,20 +1,48 @@
-import { Injectable, TemplateRef } from '@angular/core';
+import {
+  ComponentFactory,
+  ComponentFactoryResolver,
+  ComponentRef,
+  Injectable,
+  Injector,
+} from '@angular/core';
 import { ModalConfig } from '../../../interfaces/interface';
+import { ModalComponent } from './modal.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  constructor() {}
+  private componentFactory: ComponentFactory<ModalComponent>;
+
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private injector: Injector // Injetor agora é passado pelo construtor
+  ) {
+    this.componentFactory =
+      this.resolver.resolveComponentFactory(ModalComponent);
+  }
 
   public open(modalConfig: ModalConfig): ModalRef {
+    const componentRef = this.createComponentRef();
+    componentRef.instance.config = modalConfig; // Agora aplicando a configuração corretamente
+    console.log(componentRef.instance);
+
+    // Não destrua a referência do componente aqui, pois isso fechará o modal imediatamente
+    // componentRef.destroy();
     console.log('open called');
-    return new ModalRef();
+    return new ModalRef(componentRef);
+  }
+
+  private createComponentRef(): ComponentRef<ModalComponent> {
+    return this.componentFactory.create(this.injector);
   }
 }
 
 export class ModalRef {
+  constructor(private componentRef: ComponentRef<ModalComponent>) {}
+
   public close(): void {
     console.log('close called');
+    this.componentRef.destroy();
   }
 }
